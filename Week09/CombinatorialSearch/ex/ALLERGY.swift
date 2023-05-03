3618d846a64dae1642fdfeda1b612e46866ce95b
chanhihi <chanhihi55@gmail.com>
2023-05-03 22:34:14 +0900

    feat(Combi&Search): week09

--- Week09/CombinatorialSearch/ex/ALLERGY.swift
+++ Week09/CombinatorialSearch/ex/ALLERGY.swift
@@ -1,1 +1,66 @@
-// 
+var n: Int
+var m: Int
+// eaters[food] = food를 먹을 수 잇는 친구들의 번호
+var eaters = Array(repeating: 0, count: 50)
+var best = 0
+// food 이번에 고려해야 할 음식의 번호
+// edible 지금까지 고른 음식 중 i번 친구가 먹을 수 있는 음식의 수
+// chosen 지금까지 고른 음식의 수
+
+func slowSearch(food: Int, edible: [Int], chosen: Int) {
+//	간단한 가지치기
+	if chosen >= best { return }
+//	기저사례: 모든 음식에 대해 만들지 여부를 결정했으면
+//	모든 친구가 음식을 먹을 수 있는지 확인하고 그렇다면 최적해를 갱신한다.
+	if food == m {
+		if edible.firstIndex(of: 0) != nil {
+			best = chosen
+			return
+		}
+	}
+	// food를 만들지 않는 경우
+	slowSearch(food: food+1, edible: edible, chosen: chosen)
+	// 만드는 경우
+	for j in 0..<eaters[food].count {
+		edible[eaters[food][j]] += 1
+	}
+	slowSearch(food: food+1, edible: edible, chosen: chosen+1)
+    for j in 0..<eaters[food].count {
+		edible[eaters[food][j]] -= 1
+	}
+}
+
+
+// MARK: - 
+
+var canEat = Array(repeating: 0, count: 50)
+var eaters = Array(repeating: 0, count: 50)
+
+var best = 0
+
+// chosen 지금까지 선택한 음식의 수
+// edible[i] 지금까지 고른 음식 중 i번 친구가 먹을 수 있는 음식의 수
+
+func search(edible: [Int], chosen: Int) {
+	// 간단한 가지치기
+	if chosen >= best { return }
+	var first = 0
+
+	while first < n && edible[first] > 0 {first += 1}
+	// 모든 친구가 먹을 음식이 있는 경우 종료한다.
+	if first == n {
+		best = chosen
+		return 
+	}
+	// 이 친구가 먹을 수 있는 음식을 하나 만든다.
+	for i in 0..<canEat[first].count {
+		var food = canEat[first][i]
+		for j in 0..<eaters[food].count {
+			edible[eaters[food][j]] += 1
+		}
+		search(edible: edible, chosen: chosen+1)
+		for j in 0..<eaters[food].count {
+			edible[eaters[food][j]] -= 1
+		}
+	}
+}
--- Week09/CombinatorialSearch/ex/KAKURO2.swift
+++ Week09/CombinatorialSearch/ex/KAKURO2.swift
@@ -1,1 +1,73 @@
-// 
+// 비트마스크를 이용한 카쿠로의 후보 구하기
+
+// mask에 속한 원소들의 개수를 반환한다.
+func getSize(mask: Int) -> Int {
+	return mask
+}
+// mask에 속한 원소들의 합을 반환한다.
+func getSum(mask: Int) -> Int {
+	return mask
+}
+// len칸의 합이 sum이고, 이칸들에 이미 쓰인 수의 집합이 known일 때
+// 나머지 칸에 들어갈 수 있는 숫자의 집합을 반환한다.
+
+func getCandidates(len: Int, sum: Int, known: Int) -> Int {
+	//	조건에 부합하는 집합들의 교집합
+	var allSets = 0
+	// 1~9의 부분집합(즉 모든 짝수을 모두 생성하고, 그 중
+	for set in stride(from: 0, to: 1024, by: +2) {
+		// known을 포함하고 크기가 len이며 합이 sum인 집합을 모두 찾는다.
+		if set & known == known && getSize(mask: set) == len && getSum(mask: set) == sum {
+			allSets |= set
+		}
+	}
+	// known에 이미 속한 숫자들은 답에서 제외한다.
+	return allSets & ~known
+}
+
+// candidates[len][sum][known] = getCandidates(len, sum, known)
+// 선언 및 초기화
+var candidates = Array(repeating: Array(repeating: Array(repeating: 0, count: 1024), count: 46), count: 10)
+
+// candidates[][][]를 미리 계산해둔다.
+func generateCandidates() {
+//	1 ~ 9의 부분 집합을 전부 생성
+	for set in stride(from: 0, to: 1024, by: +2) {
+//		집합의 크기와 원소의 합을 계산해둔다
+		var l = getSize(mask: set)
+		var s = getSum(mask: set)
+		var subset = set
+		while true {
+			// 숫자 하나의 합이 s이고, 이미 subset숫자가 쓰여있을 때.
+			// 전체 숫자의 집합이 set이 되도록 나머지 숫자를 채워넣을 수 있다.
+			candidates[l][s][subset] |= (set & ~subset)
+			if subset == 0 {break}
+			subset = (subset - 1) & set
+		}
+	}
+}
+
+func put(y: Int, x: Int, val: Int) {
+	// put
+	for h in 0..<2 {
+		known[hint[y][x][h]] += (1<<val)
+	}
+	vale[y][x] = val
+}
+
+func remove(y: Int, x: Int, val: Int) {
+	// remove
+	for h in 0..<2 {
+		known[hint[y][x][h]] -= (1<<val)
+	}
+	vale[y][x] = 0
+}
+
+func getCandHint(hint: Int) -> Int {
+	return candidates[length[hint]][sum[hint]][known[hint]]
+}
+
+func getCandCoord(y: Int, x: Int) -> Int {
+	return getCandHint(hint[y][x][0]) &
+			getCandHint(hint[y][x][1])
+}
